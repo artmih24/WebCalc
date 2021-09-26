60db39bd519837e69288f5b384199b2616bd359e
artmih24 <83209415+artmih24@users.noreply.github.com>
2021-09-27 02:06:14 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,18 +1,18 @@
-function input(str) {
+function Input(str) {
     document.getElementById("textbox").value += str;
 }
 
-function clearTextbox() {
+function ClearTextbox() {
     document.getElementById("textbox").value = '';
 }
 
-function backspace() {
+function Backspace() {
     var str = document.getElementById("textbox").value;
     var res = str.substring(0, str.length - 1);
     document.getElementById("textbox").value = res;
 }
 
-function solve() {
+function Solve() {
     var str = document.getElementById("textbox").value;
     var res = eval(str);
     var out = "=" + String(res);
